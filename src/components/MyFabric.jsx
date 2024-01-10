f5c538f5a467b55c7cae33fb92e862c51d049dbd
Isabella Lima de Lima <isa@DESKTOP-1RVHKD0>
2024-01-10 19:57:30 -0400

    zoom e pan config

--- src/components/MyFabric.jsx
+++ src/components/MyFabric.jsx
@@ -4,6 +4,7 @@ import { FabricContext } from "../App.js";
 
 export const useFabric = () => {
     const canvas = useContext(FabricContext);
+    const { innerWidth: width } = window
     
     const fabricRef = useCallback((element) => {
       if (!element) return canvas.current?.dispose();
@@ -15,6 +16,60 @@ export const useFabric = () => {
         msImageSmoothingEnabled: false,
         oImageSmoothingEnabled: false
       });
+      canvas.current.selection = false
+
+      var pausePanning,
+          zoomStartScale,
+          currentX,
+          currentY,
+          xChange,
+          yChange,
+          lastX,
+          lastY
+
+      if (width <= 540) {
+        canvas.current?.on({
+          'touch:gesture': function(e) {
+             
+              if (e.e.touches && e.e.touches.length == 2) {
+                  pausePanning = true;
+                  var point = new fabric.Point(e.self.x, e.self.y);
+                  if (e.self.state == "start") {
+                    /* eslint-disable-next-line no-restricted-globals */
+                      zoomStartScale = self.canvas.current?.getZoom();
+                  }
+                  var delta = zoomStartScale * e.self.scale;
+                  /* eslint-disable-next-line no-restricted-globals */
+                  self.canvas.current?.zoomToPoint(point, delta);
+                  pausePanning = false;
+              }
+          },
+          'object:selected': function() {
+              pausePanning = true;
+          },
+          'selection:cleared': function() {
+              pausePanning = false;
+          },
+          'touch:drag': function(e) {
+              if (pausePanning == false && undefined != e.e.layerX && undefined != e.e.layerY) {
+                  currentX = e.e.layerX;
+                  currentY = e.e.layerY;
+                  xChange = currentX - lastX;
+                  yChange = currentY - lastY;
+  
+                  if( (Math.abs(currentX - lastX) <= 50) && (Math.abs(currentY - lastY) <= 50)) {
+                      var delta = new fabric.Point(xChange, yChange);
+                      canvas.relativePan(delta);
+                  }
+  
+                  lastX = e.e.layerX;
+                  lastY = e.e.layerY;
+              }
+          }
+      });
+
+      }
+      
       // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return fabricRef;
@@ -22,47 +77,6 @@ export const useFabric = () => {
   
 function MyFabric() {
     const fabricRef = useFabric();
-/*
-    useEffect(() => {
-
-      canvas.current?.on({
-        'touch:gesture': function(e) {
-            if (e.e.touches && e.e.touches.length == 2) {
-                pausePanning = true;
-                var point = new fabric.Point(e.self.x, e.self.y);
-                if (e.self.state == "start") {
-                    zoomStartScale = self.canvas.getZoom();
-                }
-                var delta = zoomStartScale * e.self.scale;
-                self.canvas.zoomToPoint(point, delta);
-                pausePanning = false;
-            }
-        },
-        'object:selected': function() {
-            pausePanning = true;
-        },
-        'selection:cleared': function() {
-            pausePanning = false;
-        },
-        'touch:drag': function(e) {
-            if (pausePanning == false && undefined != e.e.layerX && undefined != e.e.layerY) {
-                currentX = e.e.layerX;
-                currentY = e.e.layerY;
-                xChange = currentX - lastX;
-                yChange = currentY - lastY;
-
-                if( (Math.abs(currentX - lastX) <= 50) && (Math.abs(currentY - lastY) <= 50)) {
-                    var delta = new fabric.Point(xChange, yChange);
-                    canvas.relativePan(delta);
-                }
-
-                lastX = e.e.layerX;
-                lastY = e.e.layerY;
-            }
-        }
-    });
-
-    }, [])*/
 
     return <canvas ref={fabricRef} />
 }
