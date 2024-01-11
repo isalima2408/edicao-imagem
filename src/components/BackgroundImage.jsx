b13c8c12508b234cf20fd92a95621d9dce713d0a
Isabella Lima <149109430+isalima2408@users.noreply.github.com>
2024-01-10 20:16:08 -0400

    zoom
    zoom

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -54,6 +54,11 @@ const BackgroundImage = () => {
                 } else if (img.height == img.width) {
                     scale = scaleH
                 }
+
+
+
+
+
             } else {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
                 if (img.width > img.height && img.height < height) {
                     scale = scaleW
@@ -67,6 +72,59 @@ const BackgroundImage = () => {
                 } else if (img.height == img.width) {
                     scale = scaleW
                 }
+
+
+                var pausePanning,
+                zoomStartScale,
+                currentX,
+                currentY,
+                xChange,
+                yChange,
+                lastX,
+                lastY
+
+                canvas.current.on({
+                    'touch:gesture': function(e) {
+                       
+                        if (e.e.touches && e.e.touches.length == 2) {
+                            pausePanning = true;
+                            var point = new fabric.Point(e.self.x, e.self.y);
+                            if (e.self.state == "start") {
+                              /* eslint-disable-next-line no-restricted-globals */
+                                zoomStartScale = self.canvas.current?.getZoom();
+                            }
+                            var delta = zoomStartScale * e.self.scale;
+                            /* eslint-disable-next-line no-restricted-globals */
+                            self.canvas.current?.zoomToPoint(point, delta);
+                            pausePanning = false;
+                        }
+                    },
+                    'object:selected': function() {
+                        pausePanning = true;
+                    },
+                    'selection:cleared': function() {
+                        pausePanning = false;
+                    },
+                    'touch:drag': function(e) {
+                        if (pausePanning == false && undefined != e.e.layerX && undefined != e.e.layerY) {
+                            currentX = e.e.layerX;
+                            currentY = e.e.layerY;
+                            xChange = currentX - lastX;
+                            yChange = currentY - lastY;
+            
+                            if( (Math.abs(currentX - lastX) <= 50) && (Math.abs(currentY - lastY) <= 50)) {
+                                var delta = new fabric.Point(xChange, yChange);
+                                canvas.relativePan(delta);
+                            }
+            
+                            lastX = e.e.layerX;
+                            lastY = e.e.layerY;
+                        }
+                    }
+                });
+
+
+
             }
             
             
